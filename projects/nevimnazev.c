d42a15a4b678c1903bcd5a5383711237f7ea7251
Vladimír Studenský <126342834+jogg15@users.noreply.github.com>
2026-03-13 10:30:33 +0100

    waqk5vfjwbk5

--- projects/nevimnazev.c
+++ projects/nevimnazev.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 
 #define FILENAME "cisla.txt"
+#define RESULTFILE "vysledky.txt"
+
+unsigned int powers(const int base, int exponent) {
+    unsigned int result = 1;
+    for (; exponent >  0; exponent--) {
+        result *= base;
+    }
+    return result;
+}
 
 void listEnter(FILE *fw) {
     fprintf(fw, "\n");
 }
 
+void listResult(FILE *fw, const int base, const int exponent) {
+    fprintf(fw, "%d^%d = %d\n", base, exponent, powers(base, exponent));
+}
+
+int fileClose (FILE *fw, const char *filename) {
+    if (fclose(fw) != 0) {
+        printf("Chyba pri zavirani souboru %s.\n", filename);
+    }
+    return 0;
+}
+
 int main(void) {
-    FILE *file;
-    int symbol;
-    
     int base;
     int exponent;
 
-    file = fopen(FILENAME, "r");
+    FILE *file = fopen(FILENAME, "r");
 
     if (file == NULL) {
         printf("Soubor %s se nepodarilo otevrit.\n", FILENAME);
         return 1;
     }
 
-    while (fscanf(file, "%d%d", &base, &exponent) != EOF) {
-        printf("%d^%d\n", base, exponent);
+    FILE *resultFile = fopen(RESULTFILE, "w");
+
+    if (resultFile == NULL) {
+        printf("Nepodarilo se nacist soubor %s.\n", RESULTFILE);
+    } else {
+        printf("Uspesne se nacetl soubor %s.\n", RESULTFILE);
     }
 
-    if (fclose(file) != 0) {
-        printf("Chyba pri zavirani souboru %s.\n", FILENAME);
-        return 1;
+    while (fscanf(file, "%d%d", &base, &exponent) != EOF) {
+        listResult(stdout, base, exponent);
+        listResult(resultFile, base, exponent);
     }
+
+    fileClose(file, RESULTFILE);
 
     listEnter(stdout);
     printf("Soubor byl uspesne uzavren.\n");
